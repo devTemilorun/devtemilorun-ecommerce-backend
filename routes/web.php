a1c261529d68540adcb45584e693978054e43986
ISRAEL OLAWUNI <dev.temilorun@gmail.com>
2026-09-17 02:06:36 +0100

    feat(routes): add route to trigger database seeder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+Route::get('/run-seeder-secret-xyz', function () {
+    Artisan::call('db:seed', ['--force' => true]);
+    return 'Seeded!';
 });
